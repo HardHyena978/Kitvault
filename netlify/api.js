11580f7a32ff3073e894d63cfc5e8b9909f6077b
ChakradhwajB <chakrab.nor@gmail.com>
2025-08-04 16:52:31 -0700

    Revert "netlify"
    This reverts commit 8655f0ef3140c8acb242f222dbcbae1a0818c357.

--- netlify/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../../backend/server"); // Import your existing express app
-
-// Wrap the app in the serverless handler
-module.exports.handler = serverless(app);
