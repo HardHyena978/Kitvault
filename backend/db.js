20ad23314ad3224657bd133733a17320233cc187
ChakradhwajB <chakrab.nor@gmail.com>
2025-08-11 10:47:20 -0700

    Fixed db.js
    enable SSL for production database connection

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg");
 
 // Load environment variables from our .env file
 require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
 
 // A "Pool" is an efficient way to manage multiple client connections to the database.
 // It will automatically handle opening and closing connections.
@@ -14,6 +15,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 // We export the pool so we can import it and use it in other files.
